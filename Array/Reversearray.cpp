6e966b3b3af1782bd722555a970476928d180c71
Vasu <129766426+VasuBhalani@users.noreply.github.com>
2024-09-12 08:30:47 +0530

    Update Reversearray.cpp

--- Array/Reversearray.cpp
+++ Array/Reversearray.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+using namespace std;
+void revrese(int arr[],int i){
+int s = i;
+int e = arr.size()-1;
+while(s<=e)
+{
+  swap(arr[s++],arr[e--]);
+}
 
+void display(int arr[],int n)
+{
+  for(int i=0;i<n;i++)
+      cout<<arr[i]<<" ";
+ 
+}
+int main()
+{
+  int n;
+  cin>>n;
+
+  int arr[n];
+  for(int i=0;i<n;i++)
+  cin>>arr[i];
+
+  display(arr,n);
+  
+  revrese(arr,0);
+
+  display(arr,n);
+  return 0;
+}
