35f6e9e1c05e6c31bf211f3403fe5065f1289af8
Vasu <129766426+VasuBhalani@users.noreply.github.com>
2024-10-22 20:47:33 +0530

    Update pattern19.cpp

--- Pattern/Pattern/pattern19.cpp
+++ Pattern/Pattern/pattern19.cpp
@@ -11,7 +11,20 @@
 ****    ****
 *****  *****
 ************
+  n-i
+  [ star , space , star ]
+0 |   5      0      5
+1 |   4      2      4
+2 |   3      4      3
+3 |   2      6      2
+4 |   1      8      1
 
+
+   1 8 1
+   2 6 2
+   3 4 3
+   4 2 4
+   5 0 5
 */
 
 #include<iostream>
@@ -22,55 +35,38 @@ int main()
     cout<<"Enter the number of rows : ";
     cin>>n;
 
-     /*
-     0
-     2
-     4
-     6
-     8
-     10
-     */
-    //top
-    for(int i=1;i<=n;i++)
-    {
-        //left
-        for(int j=0;j<=n-i;j++)
-        {
-            cout<<"*";
-        }
-        //space
-        for(int s=0;s<2*i-2;s++)
-        {
-            cout<<" ";
-        } 
-        
-        //right
-        for(int r=0;r<=n-i;r++)
-        {
-            cout<<"*";
-        }
-        cout<<endl;
-    }
-    //bottom
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<=i;j++)
-        {
-            cout<<"*";
-        }
+       int space = 0;
+       for(int i=0;i<n;i++)
+       {
+         //star 
+         for(int j=0;j<n-i;j++)
+         cout<<"*"; 
 
-        //space
-        for(int s=0;s<2*(n-i)-2;s++)
-        {
-            cout<<" ";
-        }
+         //space
+           for(int l=0;l<space;l++)
+            cout<<" ";   
 
-        for(int j=0;j<=i;j++)
-        {
-            cout<<"*";
-        } 
+          //star
+          for(int k=0;k<n-i;k++)
+          cout<<"*"; 
+ 
+          cout<<endl;
+       }
 
+     for(int i=1;i<=n;i++)
+       {
+         //star 
+         for(int j=0;j<i;j++)
+         cout<<"*"; 
 
-        cout<<endl;
-    }
+         //space
+           for(int l=0;l<2*n-2*i;l++)
+            cout<<" ";   
+
+          //star
+          for(int k=0;k<i;k++)
+          cout<<"*"; 
+
+          cout<<endl;
+       }
 }
